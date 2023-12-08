7a1be23b60587f97c5fe92eeadad73077c771bcb
hsyntes <se.hsyntes@gmail.com>
2023-12-08 16:18:29 +0200

    created navbar ui for development

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,11 @@
+import Navbar from "./ui/Navbar";
+
 const Layout = ({ children }) => {
   return (
     <>
-      <header>{/* <Navbar /> */}</header>
+      <header>
+        <Navbar />
+      </header>
       <main>{children}</main>
       <footer>{/* footer */}</footer>
     </>
