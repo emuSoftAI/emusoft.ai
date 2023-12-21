b4edd8047599809e7ad96a15649df9c804f43e87
emuSoftAI <emusoft.ai@gmail.com>
2023-12-21 21:37:33 +0200

    created HttpRequest Class

---
+++ utils/HttpRequest.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export default class HttpRequest {
+  // craete a PRIVATE variable named url
+
+  constructor() {
+    // assign BACKEND_API to url
+  }
+
+  // Run the axios on body of functions
+  async get(endpoint, payload) {}
+
+  async post(endpoint, payload) {}
+
+  async patch(endpoint, payload) {}
+
+  async delete(endpoint, payload) {}
+}
